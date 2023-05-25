a0a910abb1bb7244205ee897224dfe9e2e399319
Samucraftyt <btsamuel21@gmail.com>
2023-05-25 17:29:19 +0200

    Test-01

---
+++ src/components/TitleJsx.jsx
@@ -0,0 +1,11 @@
+import './css/TitleJsxCss.css'
+
+function TitleJsx() {
+    return(
+        <div className="container">
+            <h1>Web-Test-01</h1>
+        </div>
+    )
+};
+
+export default TitleJsx;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import TitleJsx from "./components/TitleJsx";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <>
+    <TitleJsx/>
+    <App/>
+    </>
   </React.StrictMode>,
 )
