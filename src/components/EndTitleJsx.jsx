af2053c8df3bcc21864fab0607c6c46d9cd1d3df
Samucraftyt <btsamuel21@gmail.com>
2023-05-26 10:29:48 +0200

    Update

--- src/components/EndTitleJsx.jsx
+++ src/components/EndTitleJsx.jsx
@@ -2,7 +2,7 @@ import './css/EndTitleJsxCss.css'
 
 function TitleJsx() {
     return(
-        <div className="container">
+        <div className="container2">
             <h1>© 2023-2023</h1>
         </div>
     )
--- src/components/TitleJsx.jsx
+++ src/components/TitleJsx.jsx
@@ -4,6 +4,7 @@ function TitleJsx() {
     return(
         <div className="container">
             <h1>Web-Test-01</h1>
+            <h1>Home</h1>
         </div>
     )
 };
