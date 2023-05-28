61c42a1a691918f9c3b34beca589680685fcffaf
Samucraftyt <btsamuel21@gmail.com>
2023-05-28 12:02:25 +0200

    Update

--- src/components/TitleJsx.jsx
+++ src/components/TitleJsx.jsx
@@ -1,12 +1,1 @@
 import './css/TitleJsxCss.css'
-
-function TitleJsx() {
-    return(
-        <div className="container">
-            <h1>Web-Test-01</h1>
-            <h1>Home</h1>
-        </div>
-    )
-};
-
-export default TitleJsx;
