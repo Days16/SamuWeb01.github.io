5a7407613571012dd4df35bdf3896a2fe18b0856
Samucraftyt <btsamuel21@gmail.com>
2023-05-28 12:15:21 +0200

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-import TitleJsx from "./components/TitleJsx";
-import FormJsx from "./components/FormJsx";
-import EndTitleJsx from "./components/EndTitleJsx"
+import TitleJsx from "./components/Title/TitleJsx";
+import NavbarJsx from "./components/Navbar/NavbarJsx";
+import FormJsx from "./components/Form/FormJsx";
+import EndTitleJsx from "./components/EndTitle/EndTitleJsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
-    <TitleJsx/>
+    <NavbarJsx/>
     <FormJsx/>
     <EndTitleJsx/>
     <App/>
